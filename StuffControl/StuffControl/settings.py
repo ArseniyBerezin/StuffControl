8f3199841d3599d44535de48b8d95737f435529d
ArseniyBerezin <140558666+ArseniyBerezin@users.noreply.github.com>
2024-08-28 03:22:22 +0300

    1
    1

--- StuffControl/StuffControl/settings.py
+++ StuffControl/StuffControl/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_(0*4#ovxkxqi3ty7u7eb=!-+tsk_k+0wu&hr2q(w5z48eiyn5'
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
